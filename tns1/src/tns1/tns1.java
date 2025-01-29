67e56a3a9e7dca883c33ad78767710277eb6b504
nandini0126 <nandiniyamale26@gmail.com>
2025-01-29 15:03:41 +0530

    variable

--- tns1/src/tns1/tns1.java
+++ tns1/src/tns1/tns1.java
@@ -2,6 +2,11 @@ package tns1;
 
 public class tns1 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		int num=3/2;
+		System.out.println(num);
+		double num1= 9/2;
+		System.out.println(num1);
+		float num2=9/2;
+		System.out.println(num2);
 	}
 }
